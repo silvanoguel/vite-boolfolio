7a1151680abb46de32ed75a0642ee823118e1bee
silvanoguel <silvanoguel@msn.com>
2023-06-13 15:00:09 +0200

    add:project cards

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
+export default {
+  data() {
+    return {
+      baseUrl: "http://127.0.0.1:8000",
+      projects: []
+    };
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get(`${this.baseUrl}/api/projects`).then(resp => {
+        this.projects = resp.data.results;
+      });
+    }
+  },
+  components: { ProjectCard }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h2>List of projects</h2>
+    <div class="row row-cols-3 g-3">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project" />
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use "./styles/general.scss" as *;
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    },
+    data() {
+        return {
+            baseUrl: "http://127.0.0.1:8000",
+        }
+    }
+}
+</script>
+
+<template>
+
+  <div class="card h-100">
+    <div class="card-body">
+        <h5>Title: {{ project.title }}</h5>
+        <p>Content: {{ project.content }}</p>
+    </div>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+</style>
