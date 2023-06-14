1ee3c4ca2a7bda32a0b28b72bd5cc61023dea4c4
silvanoguel <silvanoguel@msn.com>
2023-06-14 15:51:24 +0200

    add:type and technologies

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,4 +1,7 @@
 <script>
+
+import { store } from "../store";
+
 export default {
     name: 'ProjectCard',
     props: {
@@ -6,10 +9,23 @@ export default {
     },
     data() {
         return {
-            baseUrl: "http://127.0.0.1:8000",
+            store
         }
+    },
+    computed: {
+        contentPreview() {
+            if (!this.project.content) {
+                return 'No content'
+            } else if (this.project.content.length > 100) {
+                return this.project.content.substring(0, 100) + "...";
+            } else {
+                return this.project.content
+            }
+        },
     }
+
 }
+
 </script>
 
 <template>
@@ -17,7 +33,20 @@ export default {
   <div class="card h-100">
     <div class="card-body">
         <h5>Title: {{ project.title }}</h5>
-        <p>Content: {{ project.content }}</p>
+        <p>Content: {{ contentPreview }}</p>
+        <p class="text-primary">
+            <span v-if="project.type">{{ project.type.name }}</span>
+            <span v-else>No category</span>
+        </p>
+
+        <p class="technologies-list mt-1 mb-2 text-success">
+            <span v-if="project.technologies.length > 0">
+                <span v-for="technology in project.technologies" class="technology">{{ technology.name}},</span>
+            </span>
+            <span v-else>
+                <span>No technologies</span>
+            </span>
+        </p>
     </div>
   </div>
 
